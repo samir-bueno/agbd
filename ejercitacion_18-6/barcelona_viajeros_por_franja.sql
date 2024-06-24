087be9781c6cd4d347fd0f8aff92c5c2dedcbe33
samir-bueno <samirbueno585@gmail.com>
2024-06-23 21:02:16 -0300

    arregle los errores que tenia y termine

--- ejercitacion_18-6/barcelona_viajeros_por_franja.sql
+++ ejercitacion_18-6/barcelona_viajeros_por_franja.sql
@@ -1,25 +1,24 @@
 --EJERCICIO 1--
-SELECT count(DISTINCT NOMBRE_ESTACION) AS cant_estaciones_totales 
+SELECT count(NOMBRE_ESTACION) AS cant_estaciones_totales 
 FROM barcelona_viajeros_por_franja_xlsx
 
 --EJERCICIO 2--
-SELECT DISTINCT NOMBRE_ESTACION
+SELECT count(DISTINCT NOMBRE_ESTACION)
 FROM barcelona_viajeros_por_franja_xlsx
 
 --EJERCICIO 3--
-SELECT NOMBRE_ESTACION, count(VIAJEROS_SUBIDOS) as pasajeros_subidos, count(VIAJEROS_BAJADOS) as pasajeros_bajados, sum(VIAJEROS_SUBIDOS) as pasajeros_subidos_total, sum(VIAJEROS_BAJADOS) as pasajeros_bajados_total, abs(sum(VIAJEROS_BAJADOS) - sum(VIAJEROS_SUBIDOS)) as diferencia
+SELECT  NOMBRE_ESTACION, sum(VIAJEROS_SUBIDOS) as pasajeros_subidos, sum(VIAJEROS_BAJADOS) as pasajeros_bajados
 FROM barcelona_viajeros_por_franja_xlsx
 GROUP by NOMBRE_ESTACION
-order by diferencia DESC
 
 --EJERCICIO 4--
-SELECT  NOMBRE_ESTACION AS cant_estaciones_totales, sum(VIAJEROS_SUBIDOS) as pasajeros_subidos, sum(VIAJEROS_BAJADOS) as pasajeros_bajados, sum(VIAJEROS_SUBIDOS) - sum(VIAJEROS_BAJADOS) as diferencia
+SELECT NOMBRE_ESTACION,  sum(VIAJEROS_SUBIDOS) as pasajeros_subidos, sum(VIAJEROS_BAJADOS) as pasajeros_bajados, abs(sum(VIAJEROS_BAJADOS) - sum(VIAJEROS_SUBIDOS)) as diferencia
 FROM barcelona_viajeros_por_franja_xlsx
 GROUP by NOMBRE_ESTACION
 order by diferencia DESC
 
 --EJERCICIO 5--
-SELECT NOMBRE_ESTACION, count(VIAJEROS_SUBIDOS) as pasajeros_subidos, TRAMO_HORARIO FROM barcelona_viajeros_por_franja_xlsx
+SELECT NOMBRE_ESTACION, max(VIAJEROS_SUBIDOS) as pasajeros_subidos, TRAMO_HORARIO FROM barcelona_viajeros_por_franja_xlsx
 GROUP BY TRAMO_HORARIO
 ORDER BY pasajeros_subidos DESC
 
@@ -29,7 +28,7 @@ SELECT NOMBRE_ESTACION, max(VIAJEROS_SUBIDOS) as pasajeros_subidos, TRAMO_HORARI
 FROM barcelona_viajeros_por_franja_xlsx
 
 --EJERCICIO 7--
-SELECT NOMBRE_ESTACION, count(VIAJEROS_BAJADOS) as pasajeros_bajados, TRAMO_HORARIO 
+SELECT NOMBRE_ESTACION, max(VIAJEROS_BAJADOS) as pasajeros_bajados, TRAMO_HORARIO 
 FROM barcelona_viajeros_por_franja_xlsx
 GROUP BY TRAMO_HORARIO
 ORDER BY pasajeros_bajados DESC
